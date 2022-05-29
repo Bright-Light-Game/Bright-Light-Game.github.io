c9cce21ebc5b24041eac8fb19b0341464db0af71
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2022-05-29 19:04:43 +0800

    Add files via upload

--- js/updates.js
+++ js/updates.js
@@ -30,6 +30,24 @@ Updates.setArticles = function ()
     
     for (let i = 0; i < this.updatesData.length; i++)
     {
-        this.mainContent.innerHTML += `<article class="updateArticle"><div class="updateHeader"><div class="updateTitle">${this.updatesData[i].title}</div><div class="updateDate">${this.updatesData[i].date}</div></div><div class="updateContent">${this.updatesData[i].content}</div></article>`;
+        let article = document.createElement("article");
+        let header = document.createElement("div");
+        let title = document.createElement("div");
+        let date = document.createElement("div");
+        let content = document.createElement("div");
+        
+        article.classList.add("updateArticle");
+        header.classList.add("updateHeader");
+        title.classList.add("updateTitle");
+        date.classList.add("updateDate");
+        content.classList.add("updateContent");
+        
+        title.innerHTML = this.updatesData[i].title;
+        date.innerHTML = this.updatesData[i].date;
+        content.innerHTML = this.updatesData[i].content;
+        
+        header.append(title, date);
+        article.append(header, content);
+        this.mainContent.append(article);
     }
 };
